1b970ee448d252278c1dfab6f89ce98549d98b03
Raphael <raphael@localhost.localdomain>
2017-01-15 16:07:17 -0200

    projeto angular 2

---
+++ client/app/listagem/listagem.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {Http} from '@angular/http';
+
+@Component({
+    moduleId: module.id,
+    selector: 'listagem',
+    templateUrl: './listagem.component.html'
+})
+export class ListagemComponent {
+
+    fotos: Object[] = [];
+
+    constructor(http:Http){
+
+            http.get('v1/fotos')
+            //map transforma o objeto Observable<Response> que é retorno da requisição em json
+            .map(res => res.json())
+            //subscribe se inscreve no retorno da requisição tranformado em json
+            .subscribe(fotos => {
+                //o retorno ja transformado em json e atribuido a propriedade do component
+                this.fotos = fotos;
+                console.log(this.fotos);
+            }, erro => console.log(erro))
+        
+    }
+
+}
