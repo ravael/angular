071c3789678cb7a02b22bfd6ad3fbaec412c4f29
Raphael <raphael@localhost.localdomain>
2017-01-17 00:45:07 -0200

    criando servicod de foto

--- client/app/painel/painel.component.ts
+++ client/app/painel/painel.component.ts
@@ -4,7 +4,8 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'painel',
-    templateUrl: './painel.component.html'
+    templateUrl: './painel.component.html',
+    styleUrls: ['./painel.component.css']
 
 })
 export class PainelComponent implements OnInit{
